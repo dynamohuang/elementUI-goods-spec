d8feb44c94eaf62a1c2bb8fe0d79c386bdb834bd
dynamohuang <huangdingzhi@yunsom.com>
2019-03-01 10:58:56 +0800

    upgrade tag selection

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,21 @@
                     </el-option>
                 </el-select>
 
+                <span v-for="tag in Spec.SpecTagList">
+<el-tag closable :disable-transitions="false" v-show="!tag.editVisible" @click="handleEdit(tag)"
+        @close="handleClose(tag,Spec.SpecTagList)">
+  {{tag.label}}
+</el-tag>
+        <el-input class="input-edit-tag" v-show="tag.editVisible" v-model="tag.label" ref="saveTagInput" size="small"
+                  @keyup.enter.native="handleTagEditConfirm(tag)" @blur="handleTagEditConfirm(tag)"></el-input>
+</span>
 
-                <el-select v-model="Spec.SpecValueList" multiple filterable default-first-option  allow-create value-key="name"
-                           placeholder="请录入规格选项">
-                </el-select>
+                <el-input class="input-new-tag" v-if="Spec.SpecTagAddVisible" v-model="Spec.SpecTagAddValue" ref="saveTagInput" size="small"
+                          @keyup.enter.native="handleInputConfirm(Spec.SpecTagAddValue,Spec)" @blur="handleInputConfirm(Spec.SpecTagAddValue,Spec)">
+                </el-input>
+                <el-button v-else class="button-new-tag" size="small" @click="showInput(Spec)">+ 规格选项</el-button>
+
+
                 <el-button type="danger" icon="el-icon-delete" circle @click="delSpec($index)"></el-button>
             </div>
             <el-button @click="addSpec()" type="primary"><i class="el-icon-plus"></i>添加规格项</el-button>
@@ -26,13 +37,13 @@
             <h2>规格明细：</h2>
 
             <el-table
-                    :data="SpecDetailList"
+
                     style="width: 100%">
 
 
                 <el-table-column
                         v-for="Spec in SpecList"
-                        v-if="Spec.SpecName"
+                        v-if="Spec.SpecName&&Spec.SpecTagList.length > 0"
                         :label="Spec.SpecName"
                         :prop="Spec.SpecName"
                 >
@@ -83,7 +94,9 @@
                 }],
                 //用户输入数据
                 SpecList: [],
-                specDetailList: []
+//                specDetailList: [],
+
+
 
             }
         },
@@ -92,13 +105,45 @@
             addSpec: function () {
                 let newSpec = {
                     SpecName: '',
-                    SpecValueList: []
+                    SpecTagList: [],
+                    SpecTagAddVisible: false,
+                    SpecTagAddValue: ''
                 };
                 this.SpecList.push(newSpec);
             },
             delSpec: function (index) {
                 this.SpecList.splice(index, 1);
                 //:todo update table
+            },
+            handleClose(tag,SpecTagList) {
+
+                SpecTagList.splice(SpecTagList.indexOf(tag), 1);
+            },
+            handleEdit(tag) {
+                tag.editVisible = true;
+                this.$nextTick(_ => {
+                    this.$refs.saveTagInput.$refs.input.focus();
+                });
+            },
+
+            showInput(Spec) {
+                Spec.SpecTagAddVisible = true;
+                Spec.$nextTick(_ => {
+                    Spec.$refs.saveTagInput.$refs.input.focus();
+                });
+            },
+
+            handleTagEditConfirm(tag) {
+                tag.editVisible = false;
+            },
+
+            handleInputConfirm(inputValue,Spec) {
+
+                if (inputValue) {
+                    Spec.SpecTagList.push({'editVisible':false,'label':inputValue});
+                }
+                Spec.SpecTagAddVisible = false;
+                Spec.SpecTagAddValue = '';
             }
 
         },
@@ -107,7 +152,7 @@
                 let validSpecList = [];
                 this.SpecList.forEach(
                     function (element) {
-                        if(element.SpecName && element.SpecValueList.length>0){
+                        if (element.SpecName && element.SpecTagList.length > 0) {
                             validSpecList.push(element);
                         }
                     }
@@ -142,7 +187,7 @@
         padding-bottom: 0;
     }
 
-    .input-new-tag {
+    .input-new-tag,.input-edit-tag {
         width: 90px;
         margin-left: 10px;
         vertical-align: bottom;
